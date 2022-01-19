88ebbb4c0dde5712961203aaa2e572155490953f
andris2233 <52408516+andris2233@users.noreply.github.com>
2022-01-20 00:19:10 +0400

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-bitwise': 'off',
+    'no-mixed-operators': 'off',
+  },
+};
